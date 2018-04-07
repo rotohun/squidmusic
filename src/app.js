a2ead1d1efa3eb995641ef0ac4170a9a23b648f0
Ronald Hunter <ronaldtoddhunter@yahoo.com>
2018-04-07 11:42:24 -0400

    updated files

---
+++ src/app.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+class App extends React.Component{
+
+    constructor(props){
+        super(props)
+
+        this.state = {
+            message: 'Hello Ronald'
+        }
+    }
+
+    render(){
+        return(
+            <div>
+                <h1> {this.state.message} </h1>
+            </div>
+        )
+    }
+}
+
+
+export default App;
